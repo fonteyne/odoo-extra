99fa3996860a826e47b0bb7e1b1df072419745c4
dbertha <david.bertha@gmail.com>
2018-08-16 12:56:37 +0200

    fix format quote

--- runbot/runbot.py
+++ runbot/runbot.py
@@ -301,7 +301,7 @@ class runbot_repo(osv.osv):
         repo.git(['fetch', '-p', 'origin', '+refs/pull/*/head:refs/pull/*'])
 
         fields = ['refname','objectname','committerdate:iso8601','authorname','authoremail','subject','committername','committeremail']
-        fmt = "%00".join(["%("+field+")" for field in fields])
+        fmt = "'" + "%00".join(["%("+field+")" for field in fields]) + "'"
         git_refs = repo.git(['for-each-ref', '--format', fmt, '--sort=-committerdate', 'refs/heads', 'refs/pull'])
         git_refs = git_refs.strip()
 
